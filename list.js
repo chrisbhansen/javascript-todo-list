b2001384423fe7565331355c46666d27a164e4a2
Chris Hansen <chris.hansen@modernmi.com>
2017-08-19 15:07:12 -0600

    create delete function in todo list

--- list.js
+++ list.js
@@ -4,14 +4,32 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
 
 	if (input === "list") {
-		todos.forEach(function(todo){
-			console.log(todo);
-		})
-
+		listTodos();
 	} else if (input === "new") {
-		var newToDo = prompt("Enter new todo");
-		todos.push(newToDo);
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 console.log("Okay, quitting the app.");
+
+function listTodos() {
+	todos.forEach(function(todo, i){
+		console.log("**************");
+		console.log(i + ": " + todo);
+	});
+	console.log("**************");
+}
+
+function addTodo() {
+	var newToDo = prompt("Enter new todo");
+	todos.push(newToDo);
+	console.log("Added Todo");
+}
+
+function deleteTodo() {
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
